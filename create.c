0cfb879547b7ef60fa44717521a3e7128993e7fc
9968170 <9968170@gmail.com>
2025-02-16 13:09:36 +0800

    Modify create.c

--- create.c
+++ create.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Image wonder only trial organization take describe.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
